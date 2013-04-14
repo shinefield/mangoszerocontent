68347df90ca0dfa3ff685cbcf6c0ec7e096a8a51
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-04-14 17:53:42 +0200

    - Added initial structure for a clean database.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

---
+++ mysql_cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This script will turn single-line INSERT statements in the SQL dumps
+# into readable multi-line statements.
+
+# - Include common libraries and settings ------------------------------------
+source "libui.sh" || { echo "Something went wrong while sourcing library lib-ui.sh" >&2 ; exit 2; }
+source "mysql_info.sh"
+
+# - Prepare required variables -----------------------------------------------
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TMP_FOLDER=${TMPDIR-/tmp}
+# ----------------------------------------------------------------------------
+
+# - Replace VALUES ( by VALUES\n( --------------------------------------------
+find ${DIR} -name '*.sql' -type f -exec perl -pi -e 's/VALUES \(/VALUES\n\(/g' {} \;
+
+# - Replace huge insert by multi-line inserts --------------------------------
+find ${DIR} -name '*.sql' -type f -exec perl -pi -e 's/\),\(/\),\n\(/g' {} \;
+
