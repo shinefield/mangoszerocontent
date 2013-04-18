3d79a66fa0497d36df0857e42f3cd0084ec711d4
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-04-18 09:11:27 +0200

    [z0011] Use one query for inserting the accounts.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- _updates/0005_realm_init.sql
+++ _updates/0005_realm_init.sql
@@ -12,18 +12,12 @@ USE `zp_realm`;
 INSERT INTO `realmlist` VALUES
     (1, 'mangos-zero', '127.0.0.1', 8085, 0, 2, 0, 0, 0, '5875 6005');
 
+INSERT INTO `account` VALUES
 -- Create an administrator account
-INSERT INTO `account` VALUES
-    (1,'ADMINISTRATOR','a34b29541b87b7e4823683ce6c7bf6ae68beaaac',3,'','0','0','','2006-04-25 10:18:56','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0);
-
+    (1,'ADMINISTRATOR','a34b29541b87b7e4823683ce6c7bf6ae68beaaac',3,'','0','0','','2006-04-25 10:18:56','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0),
 -- Create a game master account
-INSERT INTO `account` VALUES
-    (2,'GAMEMASTER','7841e21831d7c6bc0b57fbe7151eb82bd65ea1f9',2,'','0','0','','2006-04-25 10:18:56','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0);
-
+    (2,'GAMEMASTER','7841e21831d7c6bc0b57fbe7151eb82bd65ea1f9',2,'','0','0','','2006-04-25 10:18:56','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0),
 -- Create a moderator account
-INSERT INTO `account` VALUES
-    (3,'MODERATOR','a7f5fbff0b4eec2d6b6e78e38e8312e64d700008',1,'','0','0','','2006-04-25 10:19:35','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0);
-
+    (3,'MODERATOR','a7f5fbff0b4eec2d6b6e78e38e8312e64d700008',1,'','0','0','','2006-04-25 10:19:35','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0),
 -- Create a player account
-INSERT INTO `account` VALUES
     (4,'PLAYER','3ce8a96d17c5ae88a30681024e86279f1a38c041',0,'','0','0','','2006-04-25 10:19:35','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0);
