bb2e6b8b387ca24f77c9a080d41fd22997aa495c
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-04-18 20:07:34 +0200

    [z0018] Group inserts by race.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- _updates/0010_world_playercreateinfo.sql
+++ _updates/0010_world_playercreateinfo.sql
@@ -29,90 +29,57 @@ SET @CLASS_DRUID    = 11;
 
 -- Insert creation information for player characters
 INSERT INTO `playercreateinfo` VALUES
-    -- Insert Human Warrior
     (@RACE_HUMAN, @CLASS_WARRIOR, 0, 12, -8949.95, -132.493, 83.5312, 0),
-    -- Insert Human Paladin
     (@RACE_HUMAN, @CLASS_PALADIN, 0, 12, -8949.95, -132.493, 83.5312, 0),
-    -- Insert Human Rogue
     (@RACE_HUMAN, @CLASS_ROGUE, 0, 12, -8949.95, -132.493, 83.5312, 0),
-    -- Insert Human Priest
     (@RACE_HUMAN, @CLASS_PRIEST, 0, 12, -8949.95, -132.493, 83.5312, 0),
-    -- Insert Human Mage
     (@RACE_HUMAN, @CLASS_MAGE, 0, 12, -8949.95, -132.493, 83.5312, 0),
-    -- Insert Human Warlock
-    (@RACE_HUMAN, @CLASS_WARLOCK, 0, 12, -8949.95, -132.493, 83.5312, 0),
+    (@RACE_HUMAN, @CLASS_WARLOCK, 0, 12, -8949.95, -132.493, 83.5312, 0);
 
-    -- Insert Orc Warrior
+INSERT INTO `playercreateinfo` VALUES
     (@RACE_ORC, @CLASS_WARRIOR, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Orc Hunter
     (@RACE_ORC, @CLASS_HUNTER, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Orc Rogue
     (@RACE_ORC, @CLASS_ROGUE, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Orc Shaman
     (@RACE_ORC, @CLASS_SHAMAN, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Orc Warlock
-    (@RACE_ORC, @CLASS_WARLOCK, 1, 14, -618.518, -4251.67, 38.718, 0),
+    (@RACE_ORC, @CLASS_WARLOCK, 1, 14, -618.518, -4251.67, 38.718, 0);
 
-    -- Insert Dwarf Warrior
+INSERT INTO `playercreateinfo` VALUES
     (@RACE_DWARF, @CLASS_WARRIOR, 0, 1, -6240.32, 331.033, 382.758, 6.17716),
-    -- Insert Dwarf Paladin
     (@RACE_DWARF, @CLASS_PALADIN, 0, 1, -6240.32, 331.033, 382.758, 6.17716),
-    -- Insert Dwarf Hunter
     (@RACE_DWARF, @CLASS_HUNTER, 0, 1, -6240.32, 331.033, 382.758, 6.17716),
-    -- Insert Dwarf Rogue
     (@RACE_DWARF, @CLASS_ROGUE, 0, 1, -6240.32, 331.033, 382.758, 6.17716),
-    -- Insert Dwarf Priest
-    (@RACE_DWARF, @CLASS_PRIEST, 0, 1, -6240.32, 331.033, 382.758, 6.17716),
+    (@RACE_DWARF, @CLASS_PRIEST, 0, 1, -6240.32, 331.033, 382.758, 6.17716);
 
-    -- Insert Night Elf Warrior
+INSERT INTO `playercreateinfo` VALUES
     (@RACE_NIGHT_ELF, @CLASS_WARRIOR, 1, 141, 10311.3, 832.463, 1326.41, 5.69632),
-    -- Insert Night Elf Hunter
     (@RACE_NIGHT_ELF, @CLASS_HUNTER, 1, 141, 10311.3, 832.463, 1326.41, 5.69632),
-    -- Insert Night Elf Rogue
     (@RACE_NIGHT_ELF, @CLASS_ROGUE, 1, 141, 10311.3, 832.463, 1326.41, 5.69632),
-    -- Insert Night Elf Priest
     (@RACE_NIGHT_ELF, @CLASS_PRIEST, 1, 141, 10311.3, 832.463, 1326.41, 5.69632),
-    -- Insert Night Elf Druid
-    (@RACE_NIGHT_ELF, @CLASS_DRUID, 1, 141, 10311.3, 832.463, 1326.41, 5.69632),
+    (@RACE_NIGHT_ELF, @CLASS_DRUID, 1, 141, 10311.3, 832.463, 1326.41, 5.69632);
 
-    -- Insert Undead Warrior
+INSERT INTO `playercreateinfo` VALUES
     (@RACE_UNDEAD, @CLASS_WARRIOR, 0, 85, 1676.71, 1678.31, 121.67, 2.70526),
-    -- Insert Undead Rogue
     (@RACE_UNDEAD, @CLASS_ROGUE, 0, 85, 1676.71, 1678.31, 121.67, 2.70526),
-    -- Insert Undead Priest
     (@RACE_UNDEAD, @CLASS_PRIEST, 0, 85, 1676.71, 1678.31, 121.67, 2.70526),
-    -- Insert Undead Mage
     (@RACE_UNDEAD, @CLASS_MAGE, 0, 85, 1676.71, 1678.31, 121.67, 2.70526),
-    -- Insert Undead Warlock
-    (@RACE_UNDEAD, @CLASS_WARLOCK, 0, 85, 1676.71, 1678.31, 121.67, 2.70526),
+    (@RACE_UNDEAD, @CLASS_WARLOCK, 0, 85, 1676.71, 1678.31, 121.67, 2.70526);
 
-    -- Insert Tauren Warrior
+INSERT INTO `playercreateinfo` VALUES
     (@RACE_TAUREN, @CLASS_WARRIOR, 1, 215, -2917.58, -257.98, 52.9968, 0),
-    -- Insert Tauren Hunter
     (@RACE_TAUREN, @CLASS_HUNTER, 1, 215, -2917.58, -257.98, 52.9968, 0),
-    -- Insert Tauren Shaman
     (@RACE_TAUREN, @CLASS_SHAMAN, 1, 215, -2917.58, -257.98, 52.9968, 0),
-    -- Insert Tauren Druid
-    (@RACE_TAUREN, @CLASS_DRUID, 1, 215, -2917.58, -257.98, 52.9968, 0),
+    (@RACE_TAUREN, @CLASS_DRUID, 1, 215, -2917.58, -257.98, 52.9968, 0);
 
-    -- Insert Gnome Warrior
+INSERT INTO `playercreateinfo` VALUES
     (@RACE_GNOME, @CLASS_WARRIOR, 0, 1, -6240.32, 331.033, 382.758, 0),
-    -- Insert Gnome Rogue
     (@RACE_GNOME, @CLASS_ROGUE, 0, 1, -6240.32, 331.033, 382.758, 0),
-    -- Insert Gnome Mage
     (@RACE_GNOME, @CLASS_MAGE, 0, 1, -6240.32, 331.033, 382.758, 0),
-    -- Insert Gnome Warlock
-    (@RACE_GNOME, @CLASS_WARLOCK, 0, 1, -6240.32, 331.033, 382.758, 0),
+    (@RACE_GNOME, @CLASS_WARLOCK, 0, 1, -6240.32, 331.033, 382.758, 0);
 
-    -- Insert Troll Warrior
+INSERT INTO `playercreateinfo` VALUES
     (@RACE_TROLL, @CLASS_WARRIOR, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Troll Hunter
     (@RACE_TROLL, @CLASS_HUNTER, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Troll Rogue
     (@RACE_TROLL, @CLASS_ROGUE, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Troll Priest
     (@RACE_TROLL, @CLASS_PRIEST, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Troll Shaman
     (@RACE_TROLL, @CLASS_SHAMAN, 1, 14, -618.518, -4251.67, 38.718, 0),
-    -- Insert Troll Mage
     (@RACE_TROLL, @CLASS_MAGE, 1, 14, -618.518, -4251.67, 38.718, 0);
