359a0e601ad49d7ca13c841f8c6143c234ebea48
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-09-11 10:42:48 +0200

    - Added a primary key to world version table.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- world/db_version.sql
+++ world/db_version.sql
@@ -25,7 +25,8 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_r2439_01_mangos_command` bit(1) DEFAULT NULL
+  `required_r2441_01_mangos_db_version` bit(1) DEFAULT NULL,
+  PRIMARY KEY(`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
