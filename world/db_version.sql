749fa94809c5d283e1d657251b01c26bede25e02
Daniel S. Reichenbach <daniel@kogitoapp.com>
2014-07-23 20:09:11 +0200

    Synced with latest mangos-zero server.
    Updated table definitions and data for mangos-zero z2481.
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- world/db_version.sql
+++ world/db_version.sql
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2480_xxxxx_01_mangos_scripted_event` bit(1) DEFAULT NULL,
+  `required_z2481_xxxxx_01_mangos_command` bit(1) DEFAULT NULL,
   PRIMARY KEY(`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
